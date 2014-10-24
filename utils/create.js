9b242b0a9613e9ffbc893ec51ecb15de8351f25d
Sam Couch <samcouch91@gmail.com>
2014-10-24 01:07:00 -0400

    js is silly sometimes

--- utils/create.js
+++ utils/create.js
@@ -2,7 +2,8 @@ var fs = require('fs');
 var request = require('request');
 var unzip = require('unzip');
 
-function get_dotfiles(username, repo = "dotfiles"){
+function get_dotfiles(username, repo){
+	repo = repo || "dotfiles";
 	var zip_url = 'https://github.com/'+username+'/'+repo+'/archive/master.zip';
 	request(zip_url)
 	  .pipe(fs.createWriteStream(username+'_dotfiles.zip'))
